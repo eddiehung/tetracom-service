95fad34a8853461dcd30e3814cb831ef7331a90f
thomasccp <thomasccp@msn.com>
2013-11-21 22:22:46 +0000

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,11 @@ describe User do
 	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 	it { should respond_to(:admin) }
+	it { should respond_to(:relationships) }
+	it { should respond_to(:followed_users) }
+	it { should respond_to(:following?) }
+	it { should respond_to(:follow!) }
+	it { should respond_to(:unfollow!) }
 
 	it { should be_valid }
 	it { should_not be_admin }
@@ -120,5 +125,23 @@ describe User do
 	describe "remember token" do
 		before { @user.save }
 		its(:remember_token) { should_not be_blank }
+	end
+
+	describe "following" do
+		let(:other_user) { FactoryGirl.create(:user) }
+		before do
+			@user.save
+			@user.follow!(other_user)
+		end
+
+		it { should be_following(other_user) }
+		its(:followed_users) { should include(other_user) }
+
+		describe "and unfollowing" do
+			before { @user.unfollow!(other_user) }
+
+			it { should_not be_following(other_user) }
+			its(:followed_users) { should_not include(other_user) }
+		end
 	end
 end
