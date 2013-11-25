bdc918768dc5db8651f9e66b640f80bfa66e2759
thomasccp <thomasccp@msn.com>
2013-11-25 01:10:34 +0000

    Filter out non-experts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 	before_action :signed_in_user_filter, only: [:new, :create]
 
 	def index
-		@users = User.paginate(page: params[:page], :conditions => :expertise.empty?==false )
+		@users = User.paginate(page: params[:page], :conditions => "expertise<>''" )
 	end
 
 	def show
