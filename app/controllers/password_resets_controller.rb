c6a438e27ff3bacd774f92178d7e5dca8bebe673
thomasccp <thomasccp@msn.com>
2014-04-04 00:33:18 +0100

    Test mailer

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,8 +9,8 @@ class PasswordResetsController < ApplicationController
 
 	def create
 		@user = User.find_by_email(params[:email].downcase)
-		@user.send_password_reset if @user
-		redirect_to root_path, notice: "Email sent with password reset instructions."
+		user.send_password_reset if user
+		redirect_to root_url, notice: "Email sent with password reset instructions."
 	end
 
 	def edit
@@ -22,7 +22,7 @@ class PasswordResetsController < ApplicationController
 		if @user.reset_password_sent_at < 2.hours.ago
 			redirect_to_new password_reset_path, alert: "Reset password request has expired."
 		elsif @user.update_attributes(params[:user])
-			redirect_to root_path, notice: "Password has been reset!"
+			redirect_to root_url, notice: "Password has been reset!"
 		else
 			render :edit
 		end
