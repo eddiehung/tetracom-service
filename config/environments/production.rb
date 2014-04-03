7008eaf1fec4d2c755bc2ff898b97b7994bbf814
thomasccp <thomasccp@msn.com>
2014-04-04 00:46:39 +0100

    Test mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ SampleApp::Application.configure do
 	# Ignore bad email addresses and do not raise email delivery errors.
 	# Set this to true and configure the email server for immediate delivery to raise delivery errors.
 	# config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.default_url_options = { :host => "tetracom-service.doc.ic.ac.uk" }
 	config.action_mailer.delivery_method = :sendmail
 	# Defaults to:
 	# config.action_mailer.sendmail_settings = {
