ce942c41ae6a553eaab7ec6589366fb11f0a8f0d
thomasccp <thomasccp@msn.com>
2014-04-05 14:40:11 +0100

    Allow email hidden, remove unncessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ SampleApp::Application.routes.draw do
 	resources :relationships, 	only: [:create, :destroy]
 	resources :password_resets
 
-	resources :conversations, only: [:index, :show, :new, :create] do
+	resources :conversations, only: [:index, :show] do
 		member do
 			post :new
 			post :reply
